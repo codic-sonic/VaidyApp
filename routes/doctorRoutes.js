15a7e1327480e11e22b737972607d8940699f68f
codic-sonic <nakulsri004@gmail.com>
2023-04-02 09:26:58 +0530

    oijhdvc

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Doctor from "../model/doctorSchema.js";
+import Doctor from '../model/doctorSchema.js';
 const router = express.Router();
 
 router.post("/", async(req,res)=>{
